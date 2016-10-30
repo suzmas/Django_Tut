c22843cee52d0fea76bfc31cdcbbe7e299399bdd
Suzanmsu <suzansucro@gmail.com>
2016-10-29 19:22:29 -0700

    Update base templates for rango and polls. Update static style.css

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^rango/', include('rango.urls')),
--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,4 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("This is the about page. The index page is located at <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
